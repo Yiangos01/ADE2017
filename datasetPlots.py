ec41e3a2c1c5a9ea3d347b8f1e96ea54206a4ff0
Yiangos01 <ygeorg01@cs.ucy.aac.cy>
2017-02-21 14:36:22 -0800

    plot for categories added

--- datasetPlots.py
+++ datasetPlots.py
@@ -3,15 +3,17 @@ import pandas as pd
 from matplotlib import pyplot as plt
 from matplotlib import pyplot as plt2
 from collections import Counter
+import collections
 
-csv_file=pd.read_csv("topic_data.csv",sep='\t')
+
+csv_file=pd.read_csv("dataset.csv",sep='\t')
+print len(csv_file)
 c=Counter(csv_file["lang"]).most_common(6)
 x_axis=[]
 y_axis=[]
 for val in c:
 	x_axis.append(val[0])
 	y_axis.append(val[1])
-#lang
 fig,ax = plt.subplots()
 index=np.arange(len(x_axis))
 bar_width=0.7
@@ -29,7 +31,7 @@ plt.tight_layout()
 x_axis_date=[]
 y_axis_date=[]
 dates=Counter(csv_file["date"]).most_common(7)
-#print dates
+print dates
 for val in dates:
 	x_axis_date.append(val[0])
 	y_axis_date.append(val[1])
@@ -40,18 +42,34 @@ bar_width=0.5
 opacity=0.8
 error_config={'ecolor':'0.3'}
 recults=plt.bar(index,y_axis_date,bar_width,alpha=opacity,color='b',error_kw=error_config)
-plt.xlabel('Days')
+plt.xlabel('days')
 plt.ylabel('values')
 plt.title('tweets per day')
 plt.xticks(index+0.3/1.2,x_axis_date)
 plt.legend()
 plt.tight_layout()
-plt.show()
+
 
 #categories
 x_axis_cat=[]
 y_axis_cat=[]
-categories=Counter(csv_file["category"])
+categories=[]
+categories=list(collections.Counter(csv_file["category"]).items())
 print categories
-
-
+for val in categories:
+	x_axis_cat.append(val[0])
+	y_axis_cat.append(val[1])
+#print y_axis_cat
+#print x_axis_cat
+fig,ax = plt.subplots()
+index=np.arange(len(x_axis_cat))
+opacity=0.8
+error_config={'ecolor':'0.3'}
+recults=plt.bar(index,y_axis_cat,bar_width,alpha=opacity,color='g',error_kw=error_config)
+plt.xlabel('categories')
+plt.ylabel('capacity')
+plt.title('tweets per category')
+plt.xticks(index+0.3/1.2,x_axis_cat)
+plt.legend()
+plt.tight_layout()
+plt.show()
--- importCat.py
+++ importCat.py
@@ -5,19 +5,35 @@ if __name__ == '__main__':
 	
 	conn = MySQLdb.connect(host= "localhost",user="root",passwd="px2h-r7s",db="twitter_data")
 	x = conn.cursor()
-	f=open("alltopics.csv")
+	f=open("alltopic.csv")
 	csv.reader(f)
+	i=0
 	for row in f:
+		i=i+1
 		text = row.replace('\n', ' ').replace('\r',' ')
 		cat=text.split("\t")
-		cat[1] = cat[1].replace(' ','')
-		
-		
+		if len(cat)>1:
+			if cat[1]=="ongoing events" or cat[1]=="ongoing events "  :
+				cat[1]="live events"
+				#print cat
+			elif cat[1]=="group interest" or cat[1]=="group interest " :
+				cat[1]="group interest"
+			elif cat[1]=="news" or cat[1]=="news ":
+				cat[1]="news"
+			elif cat[1]=="commemoratives" or cat[1]=="commemoratives " or  cat[1]=="commemoratives\t":
+				cat[1]="commemoratives"
+			elif  cat[1]==" ":
+				cat[1]=None
+
+				
+		else :
+			print cat
+
 		try:
 			x.execute("UPDATE `topics` SET `category`=\""+cat[1]+"\" WHERE name = \""+cat[0]+"\"")
 			conn.commit()	
-			#print "succees on topic"
+			
 		except:
-			print row
-			print conn.rollback()
+			print " "
+			#print conn.rollback()
 	f.close()
